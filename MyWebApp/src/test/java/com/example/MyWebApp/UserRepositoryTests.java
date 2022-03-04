0e517fdf4c798b9fa8a52ecd1e53ae106c1658b4
Patience Cellich(Patty) <69165600+pcellich@users.noreply.github.com>
2022-03-04 14:39:52 -0500

    three more unit testing

--- MyWebApp/src/test/java/com/example/MyWebApp/UserRepositoryTests.java
+++ MyWebApp/src/test/java/com/example/MyWebApp/UserRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -29,5 +31,44 @@ public class UserRepositoryTests {
         User savedUser = repo.save(user);
         assertThat(savedUser).isNotNull();
         assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAll() {
+        Iterable<User> users = repo.findAll();
+        assertThat(users).hasSizeGreaterThan(0);
+
+        for(User user: users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        Integer userId = 1;
+        Optional<User> optionalUser = repo.findById(userId);
+        User user = optionalUser.get();
+        user.setPassword("admin1");
+        repo.save(user);
+
+        User updatedUser = repo.findById(userId).get();
+        assertThat(updatedUser.getPassword()).isEqualTo("admin1");
+    }
+
+    @Test
+    public void testGet() {
+        Integer userId = 2;
+        Optional<User> optionalUser = repo.findById(userId);
+        assertThat(optionalUser).isPresent();
+        System.out.println(optionalUser.get());
+    }
+
+    @Test
+    public void testDelete() {
+        Integer userId = 2;
+        repo.deleteById(userId);
+        Optional<User> optionalUser = repo.findById(userId);
+        assertThat(optionalUser).isNotPresent();
+
     }
 }
